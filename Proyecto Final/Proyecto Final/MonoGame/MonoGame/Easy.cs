2acfaa57cd2bd9129719fa4e3bac0a1812176b23
42145275 <42145275@A-CRO-05>
2017-05-12 11:58:03 -0300

    Septima subida

--- Proyecto Final/Proyecto Final/MonoGame/MonoGame/Easy.cs
+++ Proyecto Final/Proyecto Final/MonoGame/MonoGame/Easy.cs
@@ -6,67 +6,64 @@ using System;
 
 namespace MonoGame
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+
     public class Easy : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         private Texture2D background;
-        private Texture2D sopa;
-        private SpriteFont font;
+        private Texture2D arm;
+        private Texture2D leg;
+        private Texture2D eyes;
+        private Texture2D head;
+        private Texture2D elbow;
+        private Texture2D mouth;
+        private SpriteFont Font;
+        private SpriteFont fontsmall;
         private Texture2D grilla;
-        private int meatloaf = 0;
         private int ampera = 0;
+        string elemento;
+        int num;
+        int posicionx;
+        int posiciony;
+        private bool SopaCreada = false;
         Random random = new Random();
+
 
         string[,] matriz = new string[8, 8];
         string[] Palabras = { "arm", "leg", "eyes", "head", "elbow", "mouth" };
-        char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
         public Easy()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 900;  
+            graphics.PreferredBackBufferHeight = 530;   
+            graphics.ApplyChanges();
             Content.RootDirectory = "Content";
         }
-
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-           /* Form MyGameForm = (Form)Form.FromHandle(Window.Handle);
-            MyGameForm.FormBorderStyle = FormBorderStyle.None;*/
-            // TODO: Add your initialization logic here
             this.IsMouseVisible = true;
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             background = Content.Load<Texture2D>("Easy");
-            sopa = Content.Load<Texture2D>("Sopa1");
-            font = Content.Load<SpriteFont>("meatloaf");
+            arm = Content.Load<Texture2D>("Body/arm");
+            leg = Content.Load<Texture2D>("Body/leg");
+            eyes = Content.Load<Texture2D>("Body/eyes");
+            head = Content.Load<Texture2D>("Body/head");
+            elbow = Content.Load<Texture2D>("Body/elbow");
+            mouth = Content.Load<Texture2D>("Body/mouth");
+            Font = Content.Load<SpriteFont>("AgentOrange");
+            fontsmall = Content.Load<SpriteFont>("small");
+
         }
         protected override void UnloadContent()
         {
 
         }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -74,53 +71,64 @@ namespace MonoGame
 
             base.Update(gameTime);
         }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            spriteBatch.Begin();
-            spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
-            
-
-            grilla = new Texture2D(graphics.GraphicsDevice,1,1);
-            grilla.SetData(new Color[] { Color.White });
-            for (float x = -7; x < 7; x++)
+            if (!SopaCreada)
             {
-                Rectangle rectangle = new Rectangle((int)(100 + x * 50 /*distancia*/),0,1, 400 /*Alto*/);
-                spriteBatch.Draw(grilla, rectangle, Color.Black);
-            }
-            for (float y = -7; y < 7; y++)
-            {
-                Rectangle rectangle = new Rectangle(0/*posicion en x*/, (int)(100 + y *50/*distancia*/), 400 /*ancho*/, 1);
-                spriteBatch.Draw(grilla, rectangle, Color.Black);
-            }
+                GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            
-            int num = random.Next(0, 26);
-            char let = (char)('a' + num);
-            int k = 50;
-            for (int i = 0; i <=7; i++)
-            { 
-                for (int j = 0; j <7 ; j++)
+                spriteBatch.Begin();
+                spriteBatch.Draw(background, new Rectangle(0, 0, 900, 530), Color.White);
+                spriteBatch.DrawString(fontsmall, "Find these body parts!", new Vector2(500, 60), Color.Black);
+                spriteBatch.Draw(arm, new Vector2(520, 200),Color.White);
+                spriteBatch.Draw(leg, new Vector2(670, 230),Color.White);
+                spriteBatch.Draw(eyes, new Vector2(520, 130),Color.White);
+                spriteBatch.Draw(head, new Vector2(620, 80),Color.White);
+                spriteBatch.Draw(elbow, new Vector2(530, 300), Color.White);
+
+
+
+                grilla = new Texture2D(graphics.GraphicsDevice, 1, 1);
+                grilla.SetData(new Color[] { Color.White });
+                for (float x = -7; x < 7; x++)
                 {
-                    spriteBatch.DrawString(font, let.ToString(), new Vector2(j,k), Color.Black);
-                    k += 50;
-                    if (k > 400)
+                    Rectangle rectangle = new Rectangle((int)(150 + x * 50 /*distancia*/), 50, 1, 400 /*Alto*/);
+                    spriteBatch.Draw(grilla, rectangle, Color.Black);
+                }
+                for (float y = -7; y < 7; y++)
+                {
+                    Rectangle rectangle = new Rectangle(50/*posicion en x*/, (int)(150 + y * 50/*distancia*/), 400 /*ancho*/, 1);
+                    spriteBatch.Draw(grilla, rectangle, Color.Black);
+                }
+
+                for (int fila = 0; fila <= 7; fila++)
+                {
+                    for (int columna = 0; columna <= 7; columna++)
                     {
-                        k = 50;
+                        num = random.Next(0, 26);
+                        char let = (char)('a' + num);
+                        spriteBatch.DrawString(Font, let.ToString(), new Vector2((fila+1)*50+10, (columna+1)*50+10), Color.Black);
+                        matriz[fila, columna] = let.ToString();
                     }
                 }
+
+                for (int fila = 0; fila <= 7; fila++)
+                {
+                    for (int columna = 0; columna <= 7; columna++)
+                    {
+                        posicionx = random.Next(0, 7);
+                        posiciony = random.Next(0, 7);
+                        matriz[posicionx, posiciony] = "";
+                        //spriteBatch.DrawString(Font, /*LLENAR*/, new Vector2(posicionx,posiciony), Color.Black);
+                    }
+                }
+
+                spriteBatch.End();
+                SopaCreada = true;
+                base.Draw(gameTime);
             }
 
-            spriteBatch.End();
-            base.Draw(gameTime);
-
-        }
+    }
 
         internal class Size
         {
