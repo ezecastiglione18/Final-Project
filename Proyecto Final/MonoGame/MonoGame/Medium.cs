0cc16ccf809957d3919083a64c7a57879b35b8b9
Lila Chami <lilachami1@hotmail.com>
2017-08-07 17:04:53 -0300

    Actualizacion 7/08

--- Proyecto Final/MonoGame/MonoGame/Medium.cs
+++ Proyecto Final/MonoGame/MonoGame/Medium.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -10,20 +10,25 @@ namespace MonoGame
 {
     class Medium : Game
     {
-        GraphicsDevice graphicsDevice;
         GraphicsDeviceManager graphics;
+        public GraphicsDevice device;
         SpriteBatch spriteBatch;
         private Texture2D background;
         private Texture2D playSound;
         private Texture2D salir;
         private Texture2D DSalir;
+        private Texture2D Text1;
+        private Texture2D Text2;
+        private Texture2D Text3;
+        private Texture2D Text4;
+        private Texture2D Text5;
+        private Texture2D Text6;
         private Rectangle si = new Rectangle(298, 305, 87, 117);
         private Rectangle no = new Rectangle(491, 305, 87, 117);
         private Rectangle boton = new Rectangle(400, 35, 500, 45);
 
         private SpriteFont Font;
         Random random = new Random();
-        bool Creado = false;
         bool SalirBool = false;
         bool Dibujar = false;
         bool escuchado = false;
@@ -32,12 +37,13 @@ namespace MonoGame
 
         int[] vUsados = new int[6] { 7, 7, 7, 7, 7, 7 };
         public static Texture2D[] Imagenes = new Texture2D[6];
-        public SoundEffect[] sonidos = new SoundEffect [6];
+        public FileStream[] sonidos = new FileStream [6];
         dbConexion Conexion = new dbConexion();
 
         public Medium()
         {
             graphics = new GraphicsDeviceManager(this);
+            device = graphics.GraphicsDevice;
             graphics.PreferredBackBufferWidth = 900;
             graphics.PreferredBackBufferHeight = 530;
             graphics.ApplyChanges();
@@ -48,6 +54,26 @@ namespace MonoGame
         {
             this.IsMouseVisible = true;
             base.Initialize();
+            Conexion.Seleccionar();
+
+            #region inicializar_texturas
+            FileStream file1 = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[0].imagen, FileMode.Open);
+            FileStream file2 = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[1].imagen, FileMode.Open);
+            FileStream file3 = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[2].imagen, FileMode.Open);
+            FileStream file4 = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[3].imagen, FileMode.Open);
+            FileStream file5 = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[4].imagen, FileMode.Open);
+            FileStream file6 = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[5].imagen, FileMode.Open);
+            Text1 = Texture2D.FromStream(GraphicsDevice, file1);
+            Text2 = Texture2D.FromStream(GraphicsDevice, file2);
+            Text3 = Texture2D.FromStream(GraphicsDevice, file3);
+            Text4 = Texture2D.FromStream(GraphicsDevice, file4);
+            Text5 = Texture2D.FromStream(GraphicsDevice, file5);
+            Text6 = Texture2D.FromStream(GraphicsDevice, file6);
+            //FileStream sonido = new FileStream(Properties.Settings.Default.Ruta + "\\" + Conexion.listAnimales[i].sonido, FileMode.Open);
+            //sonido = SoundEffect.FromStream(sonido);
+            #endregion
+
+
         }
         protected override void LoadContent()
         {
@@ -96,15 +122,10 @@ namespace MonoGame
 
             #endregion
 
-            /*Conexion.Seleccionar();
-
-            FileStream file = new FileStream(Conexion.listAnimales[0].imagen, FileMode.Open);
-            Texture2D Hola = Texture2D.FromStream(graphicsDevice, file);
-            spriteBatch.Draw(Hola, new Rectangle(100, 100, 100, 100), Color.White);*/
             if (boton.Contains(mousePosition) && mouseState.LeftButton == ButtonState.Pressed && !escuchado)
             {
                 Randomize();
-                sonidos[escuchar].Play();
+                //sonidos[escuchar].Play();
                 escuchado = true;
             }
 
@@ -128,8 +149,12 @@ namespace MonoGame
                 spriteBatch.Draw(playSound, new Rectangle(400, 35, 100, 80), Color.White);
                 spriteBatch.Draw(salir, new Rectangle(730, 450, 150, 75), Color.White);
                 spriteBatch.DrawString(Font, "Which animal did you hear?", new Vector2(170, 150), Color.Black);
+                spriteBatch.Draw(Text1, new Rectangle(100, 350, Conexion.listAnimales[0].ancho, 75), Color.White);
                 spriteBatch.End();
             }
+
+            spriteBatch.Begin();
+            spriteBatch.End();
             base.Draw(gameTime);
         }
 
