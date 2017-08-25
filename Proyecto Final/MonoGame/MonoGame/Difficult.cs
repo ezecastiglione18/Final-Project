a5fb061071f2a90bf930d34b2968b196fd9db6e0
EZEQUIEL MARTIN CASTIGLIONE <42646246@ort>
2017-08-25 12:04:00 -0300

    Corregir Fondo Difficult y seguir codigo

--- Proyecto Final/MonoGame/MonoGame/Difficult.cs
+++ Proyecto Final/MonoGame/MonoGame/Difficult.cs
@@ -3,14 +3,24 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Input;
 using System;
-
 namespace MonoGame
 {
     public class Difficult : Game //MEMOTEST
     {
+        GraphicsDeviceManager graphics;
+        public GraphicsDevice device;
+        private Texture2D background;
+        private SpriteFont Font;
+        public SpriteBatch spriteBatch;
         public Difficult()
         {
-
+            graphics = new GraphicsDeviceManager(this);
+            device = graphics.GraphicsDevice;
+            graphics.PreferredBackBufferWidth = 900;
+            graphics.PreferredBackBufferHeight = 530;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+            IsFixedTimeStep = false;
         }
 
         protected override void Initialize()
@@ -21,7 +31,9 @@ namespace MonoGame
 
         protected override void LoadContent()
         {
-            
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Font = Content.Load<SpriteFont>("AgentOrange");
+            background = Content.Load<Texture2D>("Difficult");
         }
 
         protected override void UnloadContent()
@@ -36,6 +48,10 @@ namespace MonoGame
 
         protected override void Draw(GameTime gameTime)
         {
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+            spriteBatch.DrawString(Font, "Search each word with its image!", new Vector2(30, 30), Color.Black);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
